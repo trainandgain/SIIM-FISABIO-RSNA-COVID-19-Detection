204a506f19d9db4d7505f5201982635c4fcc42e3
alfieroddan <alfieroddan@gmail.com>
2021-07-14 08:36:22 +0100

    resize first in transform, save memory

--- transform/gen_transform.py
+++ transform/gen_transform.py
@@ -4,14 +4,14 @@ from albumentations.pytorch.transforms import ToTensorV2, ToTensor
 
 def train(config):
     return(A.Compose([
+        A.Resize(height=config['transform']['params']['reshape_size'], width=config['transform']['params']['reshape_size'],
+            p=1.0),
         A.HorizontalFlip(p=0.5),
         A.VerticalFlip(p=0.5),
         A.OneOf([A.RandomBrightnessContrast(brightness_limit=0.4,
                                   contrast_limit=0.4, p=0.3),
                  A.RandomGamma(gamma_limit=(50, 300), eps=None, always_apply=False,
                       p=0.3)], p=0.2),
-        A.Resize(height=config['transform']['params']['reshape_size'], width=config['transform']['params']['reshape_size'],
-                 p=1.0),
         ToTensorV2(p=1.0)],
         bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}))
 
